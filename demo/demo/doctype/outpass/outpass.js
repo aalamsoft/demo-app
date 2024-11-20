3a2951494294c31ddf2695fa7978684747e9c2c4
Abaikarthik <abai.karthik@aalamsoft.com>
2024-11-20 12:39:45 +0530

    Initial Commit

---
+++ demo/demo/doctype/outpass/outpass.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, aalamsoft and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Outpass", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ demo/demo/doctype/outpass/test_outpass.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, aalamsoft and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestOutpass(FrappeTestCase):
+	pass
